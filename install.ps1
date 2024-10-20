2b7baa61fc9d512967c7837428e27c28512ec12c
wales01 <88961974+wales01@users.noreply.github.com>
2024-10-21 02:01:20 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -16,7 +16,7 @@ if ($env:PROCESSOR_ARCHITECTURE -eq 'AMD64') {
 }
 
 # Download URL Construction
-$version = "1.1"
+$version = "main"
 $base_url = "https://github.com/wales01/tlm-ch/releases/download"
 $download_url = "${base_url}/${version}/tlm_${version}_${os}_${arch}.exe"
 
