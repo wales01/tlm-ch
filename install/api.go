fe81ca427c4ba5b282ede0e984b92357bb21d40c
wales01 <88961974+wales01@users.noreply.github.com>
2024-10-21 01:14:42 +0800

    Update api.go
    修改codellama为my_codellama_model

--- install/api.go
+++ install/api.go
@@ -21,19 +21,19 @@ func (i *Install) installModelfile(name, modelfile string) error {
 func (i *Install) deployTlm() {
 	var err error
 
-	_ = spinner.New().Type(spinner.Line).Title(" Getting latest CodeLLaMa").Action(func() {
-		err = i.api.Pull(context.Background(), &ollama.PullRequest{Model: "codellama:7b"}, func(res ollama.ProgressResponse) error {
+	_ = spinner.New().Type(spinner.Line).Title("Getting latest my_codellama_model").Action(func() {
+		err = i.api.Pull(context.Background(), &ollama.PullRequest{Model: "my_codellama_model"}, func(res ollama.ProgressResponse) error {
 			return nil
 		})
 		if err != nil {
-			fmt.Println("- Installing CodeLLaMa. " + shell.Err())
+			fmt.Println("- Installing my_codellama_model. " + shell.Err())
 			os.Exit(-1)
 		}
 	}).Run()
-	fmt.Println("- Getting latest CodeLLaMa. " + shell.Ok())
+	fmt.Println("- Getting latest my_codellama_model. " + shell.Ok())
 
 	// 6. Install the modelfile (Suggest)
-	_ = spinner.New().Type(spinner.Line).Title(" Creating Modelfile for suggestions").Action(func() {
+	_ = spinner.New().Type(spinner.Line).Title("Creating Modelfile for suggestions").Action(func() {
 		err = i.installModelfile(i.suggest.Tag(), i.explain.Modelfile())
 		time.Sleep(1 * time.Second)
 		if err != nil {
@@ -45,7 +45,7 @@ func (i *Install) deployTlm() {
 	fmt.Println("- Creating Modelfile for suggestions. " + shell.Ok())
 
 	// 7. Install the modelfile (Explain)
-	_ = spinner.New().Type(spinner.Line).Title(" Creating Modelfile for explanations").Action(func() {
+	_ = spinner.New().Type(spinner.Line).Title("Creating Modelfile for explanations").Action(func() {
 		err = i.installModelfile(i.explain.Tag(), i.explain.Modelfile())
 		time.Sleep(1 * time.Second)
 		if err != nil {
